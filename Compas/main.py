fdac73561f8ba485ae64a7e089b1043b6b5f0c04
Dmitry Novikov <dredfort.42@gmail.com>
2024-12-12 22:41:42 +0000

    Compas project for coding club (Year5)

--- Compas/main.py
+++ Compas/main.py
@@ -1,4 +1,6 @@
 from microbit import *
+import music
+# import speech
 
 display.show(Image('00000:'
                    '00000:'
@@ -65,29 +67,48 @@ sleep(200)
     
 display.clear()
 
-# compass.calibrate()
+if compass.is_calibrated() == False:
+    compass.calibrate()
 
 sector = 360/8
 start = 360 - (sector/2)
 stop = (sector/2)
+
+signal = False
+music.set_tempo(bpm=250)
 
 while True:
     d = compass.heading()
     
     if (d > start and d <= 360) or (d >= 0 and d <= stop):
         display.show(Image.ARROW_N)
+        if signal == False:
+            music.play(['e'])
+            # speech.say('North')
+        signal = True
     if d > start + 1*sector - 360  and d <= stop + 1*sector:
         display.show(Image.ARROW_NE)
-    elif d > start + 2*sector - 360 and d <= stop + 2*sector:
+        signal = False
+    if d > start + 2*sector - 360 and d <= stop + 2*sector:
         display.show(Image.ARROW_E)
-    elif d > start + 3*sector - 360 and d <= stop + 3*sector:
+        signal = False
+    if d > start + 3*sector - 360 and d <= stop + 3*sector:
         display.show(Image.ARROW_SE)
-    elif d > start + 4*sector - 360 and d <= stop + 4*sector:
+        signal = False
+    if d > start + 4*sector - 360 and d <= stop + 4*sector:
         display.show(Image.ARROW_S)
-    elif d > start + 5*sector - 360 and d <= stop + 5*sector:
+        if signal == False:
+            music.play(['f'])
+            # speech.say('South')
+        signal = True
+    if d > start + 5*sector - 360 and d <= stop + 5*sector:
         display.show(Image.ARROW_SW)
-    elif d > start + 6*sector - 360 and d <= stop + 6*sector:
+        signal = False
+    if d > start + 6*sector - 360 and d <= stop + 6*sector:
         display.show(Image.ARROW_W)
-    elif d > start + 7*sector - 360 and d <= stop + 7*sector:
+        signal = False
+    if d > start + 7*sector - 360 and d <= stop + 7*sector:
         display.show(Image.ARROW_NW)
+        signal = False
+
     
